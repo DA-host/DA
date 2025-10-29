4b5fc6d0b263fabbc89aff3299cd0acfdb2f2e24
DA-host <ispaceaa@gmail.com>
2025-10-29 22:51:53 +0530

    Update and rename 1.R.txt to 1.R

---
+++ 4.R/1.R
@@ -0,0 +1,12 @@
+
+Input Vector: marks - 45, 67, 89, 90, 34, 76, 55, 29, 100, 65, 78
+PROGRAM:
+marks <- c(45,67,89,90,34,76,55,29,100,65,78)
+mean(marks)
+median(marks)
+max(marks)
+min(marks)
+sum(marks > 75)
+any(marks < 35)
+
+sort(marks, decreasing = TRUE)
